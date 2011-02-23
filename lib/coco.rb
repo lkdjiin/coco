816e1e2d8ef9e14fe0e619ab602a7cdba1d176e9
lkdjiin <xavier.nayrac@gmail.com>
2011-02-23 12:01:13 +0100

    add html_writers.rb

--- lib/coco.rb
+++ lib/coco.rb
@@ -3,6 +3,7 @@ $COCO_PATH = File.expand_path(File.dirname(__FILE__)) + '/..'
 require 'coco/formatters'
 require 'coco/coverage_stat'
 require 'coco/coverage_result'
+require 'coco/html_writers'
 require 'coverage'
 
 module Coco
@@ -13,15 +14,6 @@ Coverage.start
 at_exit do
   result = Coco::CoverageResult.new(90).result(Coverage.result)
   puts Coco::ConsoleFormatter.new(result).format
-  
-  # Generate html files
   html_files = Coco::HtmlFormatter.new(result).format
-  FileUtils.remove_dir 'coverage' if File.exist? 'coverage'
-  FileUtils.makedirs 'coverage'
-  FileUtils.copy File.join($COCO_PATH, 'template/coco.css'), 'coverage'
-  html_files.each do |filename, html|
-    f = File.new(File.join('coverage', filename.sub(Dir.pwd, '').tr('/\\', '_') + '.html'), "w")
-    f.write html
-    f.close
-  end
+  Coco::HtmlFilesWriter.new(html_files).write
 end
