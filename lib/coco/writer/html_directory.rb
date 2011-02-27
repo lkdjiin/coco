abc21806e3d6a507d85d00deb56a0cb781986f79
lkdjiin <xavier.nayrac@gmail.com>
2011-02-27 16:18:32 +0100

    images for html

--- lib/coco/writer/html_directory.rb
+++ lib/coco/writer/html_directory.rb
@@ -9,8 +9,11 @@ module Coco
     def initialize
       @coverage_dir = 'coverage'
       @css_dir = 'coverage/css'
+      @img_dir = 'coverage/img'
       css = File.join($COCO_PATH, 'template/css')
       @css_files = Dir.glob(css + '/*')
+      img = File.join($COCO_PATH, 'template/img')
+      @img_files = Dir.glob(img + '/*')
     end
     
     # Delete the coverage/ directory
@@ -20,7 +23,9 @@ module Coco
     
     def setup
       FileUtils.makedirs @css_dir
+      FileUtils.makedirs @img_dir
       FileUtils.cp @css_files, @css_dir
+      FileUtils.cp @img_files, @img_dir
     end
     
     # I list the html files from the coverage directory
