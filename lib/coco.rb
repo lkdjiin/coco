459bfaf99c18cec25a4ae9492a3c00e11b56d236
lkdjiin <xavier.nayrac@gmail.com>
2014-02-02 11:30:25 +0100

    Add :alway_run configuration key

--- lib/coco.rb
+++ lib/coco.rb
@@ -17,17 +17,20 @@ Coverage.start
 
 at_exit do
   config = Coco::Configuration.new
-  result = Coco::CoverageResult.new(config, Coverage.result)
-  covered = result.covered_from_domain
-  
-  sources = Coco::SourceLister.new(config).list
-  uncovered = Coco::UncoveredLister.new(sources, result.all_from_domain).list
-  
-  puts Coco::ConsoleFormatter.new(covered, uncovered).format(config[:single_line_report])
-  
-  html_files = Coco::HtmlFormatter.new(covered).format
-  Coco::HtmlFilesWriter.new(html_files).write
-  
-  index = Coco::HtmlIndexFormatter.new(covered, uncovered).format
-  Coco::HtmlIndexWriter.new(index).write
+  if config.user_wants_to_run?
+    result = Coco::CoverageResult.new(config, Coverage.result)
+    covered = result.covered_from_domain
+
+    sources = Coco::SourceLister.new(config).list
+    uncovered = Coco::UncoveredLister.new(sources, result.all_from_domain).list
+
+    puts Coco::ConsoleFormatter.new(covered, uncovered)
+      .format(config[:single_line_report])
+
+    html_files = Coco::HtmlFormatter.new(covered).format
+    Coco::HtmlFilesWriter.new(html_files).write
+
+    index = Coco::HtmlIndexFormatter.new(covered, uncovered).format
+    Coco::HtmlIndexWriter.new(index).write
+  end
 end
