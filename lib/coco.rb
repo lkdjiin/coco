bc5d3c4dcdf9fddc521541ef475e59029c056509
Gioele Barabucci <gioele@svario.it>
2014-05-02 15:21:13 +0200

    Do not mess with $LOAD_PATH

--- lib/coco.rb
+++ lib/coco.rb
@@ -1,6 +1,5 @@
 # -*- encoding: utf-8 -*-
 
-$LOAD_PATH.unshift File.dirname(__FILE__)
 $COCO_PATH = File.expand_path(File.expand_path(File.dirname(__FILE__)) + '/..')
 require 'coco/formatter'
 require 'coco/cover'
