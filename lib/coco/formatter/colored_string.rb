f85f3bad42d0c0abfd4ec317ac8fea8756054e6a
lkdjiin <xavier.nayrac@gmail.com>
2014-05-04 19:27:41 +0200

    Add documentation to ColoredString

--- lib/coco/formatter/colored_string.rb
+++ lib/coco/formatter/colored_string.rb
@@ -2,24 +2,33 @@
 
 module Coco
 
-  # Extend String with ANSI colorization.
+  # Public: Build String with ANSI colorization.
   # Do nothing on Windows.
   class ColoredString < String
-    
+
+    # Public: Initialize a new ColoredString object.
+    #
+    # str - A String.
     def initialize(str="")
       super(str)
     end
-    
+
+    # Public: Make a red string.
+    #
+    # Returns String ANSIfied in red.
     def red
       colorize "\033[31m"
     end
-    
+
+    # Public: Make a yellow string.
+    #
+    # Returns String ANSIfied in yellow.
     def yellow
       colorize "\033[33m"
     end
-    
+
     private
-    
+
     def colorize(color_code)
       if RUBY_PLATFORM =~ /win32/
         self
@@ -27,7 +36,5 @@ module Coco
         "#{color_code}#{self}\033[0m"
       end
     end
-    
   end
-  
 end
