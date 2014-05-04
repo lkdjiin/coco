d01819e6d1e0cc21139fe8c12f3d68c47e9d1485
lkdjiin <xavier.nayrac@gmail.com>
2014-05-04 18:54:32 +0200

    Add documentation to HtmlDirectory

--- lib/coco/writer/html_directory.rb
+++ lib/coco/writer/html_directory.rb
@@ -2,10 +2,11 @@
 
 module Coco
 
-  # I prepare the coverage/ directory for html files.
+  # Public: I prepare the coverage/ directory for html files.
   class HtmlDirectory
     COVERAGE_DIR = 'coverage'
 
+    # Public: Initialize a new HtmlDirectory object.
     def initialize
       css = File.join($COCO_PATH, 'template/css')
       @css_files = Dir.glob(css + '/*')
@@ -13,15 +14,25 @@ module Coco
       @img_files = Dir.glob(img + '/*')
     end
 
+    # Public: Get the name of the directory where the HTML report is
+    # stored.
+    #
+    # Returns String.
     def coverage_dir
       COVERAGE_DIR
     end
 
-    # Delete the coverage/ directory
+    # Public: Delete the directory where the HTML report is stored.
+    #
+    # Returns nothing.
     def clean
       FileUtils.remove_dir(coverage_dir) if File.exist?(coverage_dir)
     end
 
+    # Public: Make all directories needed to store the HTML report, then
+    # copy media files (css, images, etc.).
+    #
+    # Returns nothing.
     def setup
       FileUtils.makedirs(css_dir)
       FileUtils.makedirs(image_dir)
@@ -29,7 +40,10 @@ module Coco
       FileUtils.cp(@img_files, image_dir)
     end
 
-    # I list the html files from the coverage directory
+    # Public: I list the html files from the directory where the HTML
+    # report is stored.
+    #
+    # Returns nothing.
     def list
       files = Dir.glob("#{coverage_dir}/*.html")
       files.map {|file| File.basename(file) }
