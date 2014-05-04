12344e40d9cdd82fcf529425f55d2a38977fe4dd
lkdjiin <xavier.nayrac@gmail.com>
2014-05-04 17:27:36 +0200

    Add documentation

--- lib/coco/cover/coverage_stat.rb
+++ lib/coco/cover/coverage_stat.rb
@@ -2,18 +2,24 @@
 
 module Coco
 
-  # Give statistics about an array of lines hit.
+  # Public: Give statistics about an array of lines hit.
   #
   # An "array of lines hit" is an array of integers, possibly nil.
   # Such array is obtain from Coverage.result.
   #
   # Each integer represent the state of a source line:
-  # * nil: source line will never be reached (like comments)
-  # * 0: source line could be reached, but was not
-  # * 1 and above: number of time the source line has been reached
+  # * nil: source line will never be reached (like comments).
+  # * 0: source line could be reached, but was not.
+  # * 1 and above: number of time the source line has been reached.
   module CoverageStat
     extend self
 
+    # Public: Compute the percentage of code coverage for a file.
+    # The file is represented by an array of hits.
+    #
+    # hits - Array of Integer.
+    #
+    # Returns a Integer (rounded) percentage.
     def coverage_percent(hits)
       hits = hits.compact
       return 0 if hits.empty?
@@ -21,6 +27,11 @@ module Coco
       (number_of_covered_lines(hits) * one_percent).to_i
     end
 
+    # Compute the total of covered lines in a hits array.
+    #
+    # hits - Array of Integer.
+    #
+    # Returns Integer.
     def number_of_covered_lines(hits)
       hits.select {|hit| hit > 0 }.size
     end
