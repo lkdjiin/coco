7aaa2b6d170306bced3ab95a394b3c627eac5405
lkdjiin <xavier.nayrac@gmail.com>
2014-05-04 18:45:14 +0200

    Refactor a bit HtmlDirectory

--- lib/coco/writer/html_directory.rb
+++ lib/coco/writer/html_directory.rb
@@ -4,36 +4,45 @@ module Coco
 
   # I prepare the coverage/ directory for html files.
   class HtmlDirectory
-    attr_reader :coverage_dir
-    
+    COVERAGE_DIR = 'coverage'
+
     def initialize
-      @coverage_dir = 'coverage'
-      @css_dir = 'coverage/css'
-      @img_dir = 'coverage/img'
       css = File.join($COCO_PATH, 'template/css')
       @css_files = Dir.glob(css + '/*')
       img = File.join($COCO_PATH, 'template/img')
       @img_files = Dir.glob(img + '/*')
     end
-    
+
+    def coverage_dir
+      COVERAGE_DIR
+    end
+
     # Delete the coverage/ directory
     def clean
-      FileUtils.remove_dir @coverage_dir if File.exist? @coverage_dir
+      FileUtils.remove_dir(coverage_dir) if File.exist?(coverage_dir)
     end
-    
+
     def setup
-      FileUtils.makedirs @css_dir
-      FileUtils.makedirs @img_dir
-      FileUtils.cp @css_files, @css_dir
-      FileUtils.cp @img_files, @img_dir
+      FileUtils.makedirs(css_dir)
+      FileUtils.makedirs(image_dir)
+      FileUtils.cp(@css_files, css_dir)
+      FileUtils.cp(@img_files, image_dir)
     end
-    
+
     # I list the html files from the coverage directory
     def list
-      files = Dir.glob("#{@coverage_dir}/*.html")
+      files = Dir.glob("#{coverage_dir}/*.html")
       files.map {|file| File.basename(file) }
     end
-    
-  end
 
+    private
+
+    def css_dir
+      "#{COVERAGE_DIR}/css"
+    end
+
+    def image_dir
+      "#{COVERAGE_DIR}/img"
+    end
+  end
 end
