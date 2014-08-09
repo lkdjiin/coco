548500e48b5e2a0c8eecb61266215ad1c00edc26
lkdjiin <xavier.nayrac@gmail.com>
2014-08-09 10:22:45 +0200

    Split formatters_spec

--- spec/formatters/console_formatter_spec.rb
+++ spec/formatters/console_formatter_spec.rb
@@ -3,6 +3,7 @@
 require './spec/helper'
 
 describe ConsoleFormatter do
+
   it "should respond to format" do
     formatter = ConsoleFormatter.new(COVERAGE_90, ['a', 'b', 'c'], 100)
     formatter.respond_to?(:format).should == true
@@ -50,6 +51,7 @@ describe ConsoleFormatter do
   end
 
   context "when 'single_line_report' is true" do
+
     context "and there is some uncovered files" do
       it "should return a message" do
         formatter = ConsoleFormatter.new(COVERAGE_90, ['a', 'b', 'c'], 100)
@@ -57,6 +59,7 @@ describe ConsoleFormatter do
         result.should == "\e[33mSome files are uncovered\e[0m"
       end
     end
+
     context "and there is no uncovered files" do
       it "should return nothing" do
         formatter = ConsoleFormatter.new(COVERAGE_90, [], 100)
@@ -64,70 +67,7 @@ describe ConsoleFormatter do
         result.should == ""
       end
     end
-  end
-end
 
-describe HtmlFormatter do
-
-  it "should respond to format" do
-    formatter = HtmlFormatter.new COVERAGE_70
-    formatter.respond_to?(:format).should == true
   end
 
-  it "should return the right number of html file(s)" do
-    formatter = HtmlFormatter.new COVERAGE_30_70
-    result = formatter.format
-    result.size.should == 2
-  end
-
-  it "should return html file(s)" do
-    formatter = HtmlFormatter.new COVERAGE_70
-    result = formatter.format
-    result.each {|k, v| v.start_with?('<!DOCTYPE html>').should == true }
-  end
-
-  # Bug 13
-  it "should produce html entities for < and >" do
-    file = File.join(Coco::ROOT, 'spec/project/html_entities.rb')
-    coverage = {file => [1, 1, 0, nil, 1, 1, nil, nil]}
-    formatter = HtmlFormatter.new coverage
-    result = formatter.format[file]
-    result.match(/a &lt; b<\/pre><\/td>/).should_not == nil
-    result.match(/a &gt; b<\/pre><\/td>/).should_not == nil
-  end
-
-end
-
-describe HtmlIndexFormatter do
-  it "should respond to format" do
-    formatter = HtmlIndexFormatter.new(COVERAGE_30_70, [])
-    formatter.respond_to?(:format).should == true
-  end
-
-  it "should build the index.html" do
-    formatter = HtmlIndexFormatter.new(COVERAGE_30_70, [])
-    formatter.format.start_with?('<!DOCTYPE html>').should == true
-  end
-end
-
-describe ColoredString do
-  it "should act as a string" do
-    instance = ColoredString.new
-    instance.kind_of?(String).should == true
-  end
-
-  it "should accept a string on instanciation" do
-    instance = ColoredString.new 'azerty'
-    instance.should == 'azerty'
-  end
-
-  it "should redify a string" do
-    string = ColoredString.new 'azerty'
-    string.red.should == "\e[31mazerty\e[0m"
-  end
-
-  it "should yellowify a string" do
-    string = ColoredString.new 'azerty'
-    string.yellow.should == "\e[33mazerty\e[0m"
-  end
 end
