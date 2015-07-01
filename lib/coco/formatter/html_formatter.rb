61c7c390c8844a05b5f8d2d6b82dbfde4664f5fb
lkdjiin <xavier.nayrac@gmail.com>
2015-07-01 16:38:10 +0200

    A bit of cleaning

--- lib/coco/formatter/html_formatter.rb
+++ lib/coco/formatter/html_formatter.rb
@@ -13,12 +13,12 @@ module Coco
       super(raw_coverages, [])
       @formatted_output_files = {}
       @context = nil
-      @template = Template.open File.join(Coco::ROOT, 'template/file.erb')
+      @template = Template.open(File.join(Coco::ROOT, 'template/file.erb'))
     end
 
     def format
       @raw_coverages.each do |filename, coverage|
-        build_html filename, coverage
+        build_html(filename, coverage)
       end
       @formatted_output_files
     end
@@ -26,13 +26,16 @@ module Coco
     private
 
     def build_html(filename, coverage)
-      source = File.readlines filename
       lines = []
-      source.each_with_index do |line, index|
+      source(filename).each_with_index do |line, index|
         lines << [index+1, CGI.escapeHTML(line.chomp), coverage[index]]
       end
-      @context = Context.new filename, lines
+      @context = Context.new(filename, lines)
       @formatted_output_files[filename] = @template.result(@context.get_binding)
+    end
+
+    def source(filename)
+      File.readlines(filename)
     end
 
   end
