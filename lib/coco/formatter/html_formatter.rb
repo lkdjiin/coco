6345bdc0bf46d28dbaafb1b90ae96f7fc9cf394f
lkdjiin <xavier.nayrac@gmail.com>
2015-07-02 15:25:32 +0200

    Remove base path in files title

--- lib/coco/formatter/html_formatter.rb
+++ lib/coco/formatter/html_formatter.rb
@@ -30,7 +30,7 @@ module Coco
       source(filename).each_with_index do |line, index|
         lines << [index+1, CGI.escapeHTML(line.chomp), coverage[index]]
       end
-      @context = Context.new(filename, lines)
+      @context = Context.new(Helpers.name_for_html(filename), lines)
       @formatted_output_files[filename] = @template.result(@context.get_binding)
     end
 
